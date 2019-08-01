8996c56e2df4ad0c3bece21a4d1048907dd4d067
tdrydendiaz <tdrydendiaz@gmail.com>
2019-08-01 12:22:16 +0100

    delete working

--- src/Components/MoreDetail.js
+++ src/Components/MoreDetail.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
-
+import axios from "axios";
 
 
 export class MoreDetail extends Component {
+
+        constructor() {
+            super();
+            this.state = {
+                confirm: ""
+            };
+        }
+makeRequest = () => {
+        let deleteItem = {
+            _id: this.props.id
+        }
+        axios
+            .delete("http://localhost:5000/recipe/deleteRecipe", { data: deleteItem })
+            .then(response => {
+                console.log("deleted recipe")
+this.setState({
+          text: "deleted"
+          
+        });
+                this.props.getAll();
+            });
+    };
+
+
+
 
     render() {
 
@@ -12,7 +37,7 @@ export class MoreDetail extends Component {
                 <p>Recipe Description: {this.props.description}</p>
                 <p>Recipe Ingrients: {this.props.ingredients}</p>
                 <p>Recipe Image: {this.props.image}</p>
-            
+                <button onClick={this.makeRequest} Delete Recipe> Delete Recipe </button>
             </div>
 
         );
